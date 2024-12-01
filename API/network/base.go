f1899f733ccf20d7f87e8fb82d0d6bbdf1fb007c
Bumgu Kang <flrhyme9@gmail.com>
2024-12-01 14:04:55 +0900

    added 2 spans

--- API/network/base.go
+++ API/network/base.go
@@ -13,13 +13,7 @@ func newRootSpan(name string, c *gin.Context) opentracing.Span {
 	spanCtx, _ := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
 	sendSpan := tracer.StartSpan(name, ext.RPCServerOption(spanCtx))
 
-	defer sendSpan.Finish()
 	return sendSpan
- }
-
-func (s *Router) send(c *gin.Context) {
-	newRootSpan("send_root_span", c)
-	c.JSON(http.StatusOK, "Success")
 }
 
 func (s *Router) defaultHandler(c *gin.Context) {
@@ -28,28 +22,26 @@ func (s *Router) defaultHandler(c *gin.Context) {
 	})
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+func (s *Router) send(c *gin.Context) {
+	sendSpan := newRootSpan("send_root_span", c)
+	defer sendSpan.Finish()
+	c.JSON(http.StatusOK, "Success")
+}
 
 
 func (s *Router) sendWithTag(c *gin.Context) {
+	rootSpan := newRootSpan("send_root_span_with_tag", c)
+	rootSpan.SetTag("tag_one", "one 입니다")
+	rootSpan.SetTag("tag_two", "two 입니다")
+	defer rootSpan.Finish()
+
+	c.JSON(http.StatusOK, "Success")
 }
 
 func (s *Router) sendWithChild(c *gin.Context) {
+	rootSpan := newRootSpan("send_root_span_with_child", c)
+	defer rootSpan.Finish()
+	childSpan := opentracing.GlobalTracer().StartSpan("child_span", opentracing.ChildOf(rootSpan.Context()))
+	defer childSpan.Finish()
+	c.JSON(http.StatusOK, c)
 }
--- API/network/router.go
+++ API/network/router.go
@@ -11,12 +11,12 @@ func newRouter(n *Network) {
 	n.Router(GET, "/send-with-tag", s.sendWithTag)
 	n.Router(GET, "/send-with-child", s.sendWithChild)
 
-	n.Router(GET, "/receive-from-other-host", s.defaultHandler)
-	n.Router(GET, "/send-other-host", s.defaultHandler)
-	n.Router(GET, "/receive-two-from-other-host", s.defaultHandler)
+	n.Router(GET, "/send-other-host", s.sendWithOtherHost)
+	n.Router(GET, "/receive-from-other-host", s.receiveOne)
+	n.Router(GET, "/receive-two-from-other-host", s.receiveTwo)
 
-	n.Router(GET, "/send-for-panic", s.defaultHandler)
-	n.Router(GET, "/receive-for-error", s.defaultHandler)
+	n.Router(GET, "/send-for-panic", s.sendForPanic)
+	n.Router(GET, "/receive-for-error", s.receiveForError)
 
 	n.Router(GET, "/send-for-baggage", s.defaultHandler)
 	n.Router(GET, "/receive-for-baggage", s.defaultHandler)
